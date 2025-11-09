35327fc04c8a19a47dfff4c881bf6c2b0f7e4c28
Jabes <108432086+Jbyanx@users.noreply.github.com>
2025-11-08 19:23:02 -0500

    renombrando dao y sus ocurrencias

--- micro-proyectos/src/main/java/com/proyectos/service/CategoriaService.java
+++ micro-proyectos/src/main/java/com/proyectos/service/CategoriaService.java
@@ -3,19 +3,19 @@ package com.proyectos.service;
 import com.proyectos.dto.GetCategoria;
 import com.proyectos.mapper.GenericMapper;
 import com.proyectos.model.Categoria;
-import com.proyectos.persistence.CategoriaDao;
+import com.proyectos.persistence.dao.CategoriaDaoPostgres;
 
 import java.util.List;
 
 public class CategoriaService {
-    private CategoriaDao categoriaDao;
+    private CategoriaDaoPostgres categoriaDaoPostgres;
     private GenericMapper<GetCategoria,Categoria> genericMapper = new GenericMapper();
     public CategoriaService() {
-        categoriaDao = new CategoriaDao();
+        categoriaDaoPostgres = new CategoriaDaoPostgres();
     }
 
     public List<GetCategoria> findAll() {
-        List<Categoria> categorias =  categoriaDao.findAll();
+        List<Categoria> categorias =  categoriaDaoPostgres.findAll();
         return categorias.stream()
                 .map(c -> genericMapper.toDto(c,GetCategoria.class))
                 .toList();
--- micro-proyectos/src/main/java/com/proyectos/service/ProyectoService.java
+++ micro-proyectos/src/main/java/com/proyectos/service/ProyectoService.java
@@ -6,16 +6,17 @@ import com.proyectos.dto.GetOrganizacion;
 import com.proyectos.mapper.GenericMapper;
 import com.proyectos.model.Categoria;
 import com.proyectos.model.Proyecto;
-import com.proyectos.persistence.CategoriaDao;
-import com.proyectos.persistence.ProyectoDao;
+import com.proyectos.persistence.dao.CategoriaDao;
+import com.proyectos.persistence.dao.CategoriaDaoPostgres;
+import com.proyectos.persistence.dao.ProyectoDaoPostgres;
 
 import java.sql.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class ProyectoService {
-    private final ProyectoDao proyectoDao= new ProyectoDao();
-    private final CategoriaDao categoriaDao = new CategoriaDao();
+    private final ProyectoDaoPostgres proyectoDaoPostgres = new ProyectoDaoPostgres();
+    private final CategoriaDao dao = new CategoriaDaoPostgres();
     private final GenericMapper<GetProyecto,Proyecto> genericMapper = new GenericMapper<>();
     private final Cliente<GetOrganizacion> cliente = new Cliente<>();
     private final Logger logger = Logger.getLogger(ProyectoService.class.getName());
@@ -23,15 +24,15 @@ public class ProyectoService {
 
     public Proyecto save(String nombre, String descripcion, String ubicacion, String requisitos, Date fechaInicio, Date fechaFin, Integer voluntarios_requeridos, Long idCategoria, Long idOrganizacion){
 
-        Categoria categoria = categoriaDao.findById(idCategoria).get();
+        Categoria categoria = dao.findById(idCategoria).get();
 
-        Proyecto nuevoProyecto = proyectoDao.save(new Proyecto(nombre,descripcion,ubicacion,requisitos,fechaInicio,fechaFin,voluntarios_requeridos,categoria,idOrganizacion)) ;
+        Proyecto nuevoProyecto = proyectoDaoPostgres.save(new Proyecto(nombre,descripcion,ubicacion,requisitos,fechaInicio,fechaFin,voluntarios_requeridos,categoria,idOrganizacion)) ;
 
         return nuevoProyecto;
     }
 
     public List<GetProyecto> findAllProyectosByOrganizacion(Long idOrganizacion){
-        List<Proyecto> proyectos = proyectoDao.findAllProyectosByOrganizacion(idOrganizacion);
+        List<Proyecto> proyectos = proyectoDaoPostgres.findAllProyectosByOrganizacion(idOrganizacion);
 
         GetOrganizacion organizacion = cliente.getById(idOrganizacion,"http://usuarios:8080/organizaciones", GetOrganizacion.class);
 
@@ -48,13 +49,13 @@ public class ProyectoService {
 
     public Proyecto update(Long id, String nombre, String descripcion, String ubicacion, String requisitos, Date fechaInicio, Date fechaFin, Integer voluntarios_requeridos, Long idCategoria){
         
-        Categoria categoria = (new CategoriaDao()).findById(idCategoria).get();
+        Categoria categoria = (new CategoriaDaoPostgres()).findById(idCategoria).get();
         Proyecto proyectoUpdate = new Proyecto(id,nombre,descripcion,ubicacion,requisitos,fechaInicio,fechaFin,voluntarios_requeridos,categoria);
-        return proyectoDao.update(proyectoUpdate);
+        return proyectoDaoPostgres.update(proyectoUpdate);
     }
 
     public GetProyecto findById(Long id) {
-        Proyecto proyecto = proyectoDao.findById(id).get();
+        Proyecto proyecto = proyectoDaoPostgres.findById(id).get();
         GetOrganizacion organizacion = cliente.getById(proyecto.getIdOrganizacion(), "http://usuarios:8080/organizaciones", GetOrganizacion.class);
 
         logger.info("Organizacion:\n"+organizacion);
